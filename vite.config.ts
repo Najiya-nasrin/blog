b1f27a1f5220a413e85c6ccb2640d405cd7c5ea0
Najiya nasrin <mails4najiya@gmail.com>
2024-05-21 23:15:07 +0300

    build config changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/blog",
   plugins: [react()],
+  build: {
+    outDir: 'build',
+  }
 })
